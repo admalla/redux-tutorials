bbc1dcaa116c195e9b9874e8161b689864b56291
admalla <maasovec@gmail.com>
2021-12-24 17:50:38 +0300

    Реализовать создание задачи

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
+import React from 'react';
 import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
 import { AddField } from './components/AddField';
 import { Item } from './components/Item';
+import { reducer } from './redux/reducer';
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    checkbox: false,
+    checkTask: false,
+    inputValue: '',
+    tasks: [],
+  });
+
+  const onChange = (string) => {
+    dispatch({
+      type: 'input/value',
+      payload: string,
+    });
+  };
+
+  const onCheck = (bull) => {
+    dispatch({
+      type: 'box/check',
+      payload: bull,
+    });
+  };
+
+  const addTask = () => {
+    dispatch({
+      type: 'task/send',
+    });
+  };
+
+  const addCheckTask = (bull) => {
+    dispatch({
+      type: 'task/check',
+      payload: bull,
+    });
+  };
+
+  console.log(state);
+
   return (
     <div className="App">
       <Paper className="wrapper">
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField />
+        <AddField
+          onChange={onChange}
+          onCheck={onCheck}
+          text={state.inputValue}
+          onBox={state.checkbox}
+          addTask={addTask}
+        />
         <Divider />
         <Tabs value={0}>
           <Tab label="Все" />
@@ -18,7 +62,17 @@ function App() {
         </Tabs>
         <Divider />
         <List>
-          <Item text="Задача №1" />
+          {state.tasks
+            .sort((a, b) => a.id - b.id)
+            .map((obj) => (
+              <Item
+                key={obj.id}
+                text={obj.text}
+                completed={obj.completed}
+                addCheckTask={addCheckTask}
+                checkTask={state.checkTask}
+              />
+            ))}
         </List>
         <Divider />
         <div className="check-buttons">
---
+++ src/redux/reducer.js
@@ -0,0 +1,39 @@
+export function reducer(state, action) {
+  if (action.type === 'input/value') {
+    return {
+      ...state,
+      inputValue: action.payload,
+    };
+  }
+
+  if (action.type === 'box/check') {
+    return {
+      ...state,
+      checkbox: action.payload,
+    };
+  }
+
+  if (action.type === 'task/send') {
+    return {
+      ...state,
+      tasks: [
+        ...state.tasks,
+        {
+          id: state.tasks.length ? state.tasks[state.tasks.length - 1].id + 1 : 0,
+          text: state.inputValue,
+          completed: state.checkbox,
+        },
+      ],
+      inputValue: '',
+      checkbox: false,
+    };
+  }
+
+  if (action.type === 'task/check') {
+    return {
+      ...state,
+      checkTask: action.payload,
+    };
+  }
+  return state;
+}
